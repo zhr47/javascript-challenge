f1df4f788a89804c5f72caa218cb19454a0a4a30
Zaheer Rizvi <zhr47@Zaheers-MacBook-Pro.local>
2020-10-03 08:18:26 -0500

    changes

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,14 @@
 // from data.js
 var tableData = data;
+var len = tableData.length;
+console.log(len);
 
 // YOUR CODE HERE!
+
+var filterbutton = d3.select("#filterbutton");
+
+// 1. write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
+// 2. Make sure you have a column for `date/time`, `city`, `state`, `country`, `shape`, and `comment` at the very least.
+// 3. Use a date form in your HTML document and write JavaScript code that will listen for events and search through the `date/time` column 
+//    to find rows that match user input.
+
